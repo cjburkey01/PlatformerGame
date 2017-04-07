818f11e9d2b31b44da5616d21f084135a84880bb
CJ Burkey <cjburkey01@gmail.com>
2017-04-07 18:00:38 -0400

    Movement

---
+++ Platformer/Assets/Script/CameraFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject obj;
+	public float damping = 0.3f;
+	public float zoomDamping = 0.5f;
+	public float defaultZoom = 5f;
+	public float movingZoom = 10f;
+
+	private float zoom = 5f;
+	private Vector3 vel = Vector3.zero;
+	private float r = 0f;
+
+	void FixedUpdate() {
+		// Smooth Movement
+		Vector3 toPos = obj.transform.position;
+		toPos.z = -10;
+		transform.position = Vector3.SmoothDamp(transform.position, toPos, ref vel, damping);
+
+		// Smooth Zoom
+		float targetZoom = defaultZoom;
+		if(obj.GetComponent<Rigidbody2D>() != null) {
+			Vector3 vel = obj.GetComponent<Rigidbody2D>().velocity;
+			if(vel.x != 0 || vel.y != 0 || vel.z != 0) {
+				targetZoom = movingZoom;
+			}
+		}
+		GetComponent<Camera>().orthographicSize = Mathf.SmoothDamp(zoom, targetZoom, ref r, zoomDamping);
+	}
+
+}
--- Platformer/Assets/Script/PlayerControl.cs
+++ Platformer/Assets/Script/PlayerControl.cs
@@ -6,8 +6,6 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour {
 
 	private Rigidbody2D rb;
-	//private BoxCollider2D coll;
-
 	private bool grounded = false;
 
 	public float gravity = 0.2f;
@@ -17,16 +15,23 @@ public class PlayerControl : MonoBehaviour {
 
 	void Start() {
 		rb = GetComponent<Rigidbody2D>();
-		//coll = GetComponent<BoxCollider2D>();
 	}
 
-	void Update() {
+	void FixedUpdate() {
+		// Velocity modification
 		Vector2 velocity = rb.velocity;
 		velocity.y -= gravity;
 		if(velocity.y < -maxYVel) velocity.y = -maxYVel;
 		velocity.x = Input.GetAxis("Horizontal") * moveSpeed;
 		if(Input.GetAxisRaw("Vertical") > 0 && grounded) { rb.AddForce(new Vector2(0, jumpForce)); }
 		rb.velocity = velocity;
+
+		// Rotate player
+		if(Input.GetAxis("Horizontal") > 0) {
+			transform.rotation = Quaternion.Euler(0, 0, 0);
+		} else if(Input.GetAxis("Horizontal") < 0) {
+			transform.rotation = Quaternion.Euler(0, 180, 0);
+		}
 	}
 
 	void OnCollisionStay2D(Collision2D c) {
