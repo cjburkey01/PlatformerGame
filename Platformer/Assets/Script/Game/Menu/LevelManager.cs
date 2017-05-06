95ab982e3e8147dbb28a2c8fe5bbcdd5936cba91
aeisley719 <eisley.alex@gmail.com>
2017-05-06 13:29:57 -0700

    trying to do some stuff and failing.

--- Platformer/Assets/Script/Game/Menu/LevelManager.cs
+++ Platformer/Assets/Script/Game/Menu/LevelManager.cs
@@ -1,16 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnMouseDown()
+	{
+		SceneManager.LoadScene (name);
 	}
 }
