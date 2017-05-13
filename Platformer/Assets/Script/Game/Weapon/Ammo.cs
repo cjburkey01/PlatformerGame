70148f16dd60d6013b8d7b568ee87d05b6c5eda7
aeisley719 <eisley.alex@gmail.com>
2017-05-12 21:30:11 -0700

    ammo

--- Platformer/Assets/Script/Game/Weapon/Ammo.cs
+++ Platformer/Assets/Script/Game/Weapon/Ammo.cs
@@ -4,13 +4,5 @@ using UnityEngine;
 
 public class Ammo : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public int Ammo = 20;
 }
