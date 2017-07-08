c15e1f46c7d370140628da78b17577c635b6e249
CJ Burkey <cjburkey01@gmail.com>
2017-07-08 19:47:22 -0400

    Map loading

--- Platformer/Assets/Script/Game/Level/LevelLoader.cs
+++ Platformer/Assets/Script/Game/Level/LevelLoader.cs
@@ -15,7 +15,24 @@ public class LevelLoader : MonoBehaviour {
 		int width = textureMap.width;
 		int height = textureMap.height;
 
-		for(int x = 0; x < width; x ++) {
+		Color32[] colors = textureMap.GetPixels32();
+
+		for(int i = 0; i < colors.Length; i ++) {
+			Color32 colorAt = colors[i];
+			int x = i % width;
+			int y = Mathf.FloorToInt((float) i / (float) width);
+			Tile tile = getTile(colorAt);
+			if(tile != null) {
+				Vector3 pos = new Vector3(x, y - (width - 1), 0.0f);
+				if(!tile.player) {
+					Instantiate(tile.obj, pos, Quaternion.identity);
+				} else {
+					tile.obj.transform.position = pos;
+				}
+			}
+		}
+
+		/*for(int x = 0; x < width; x ++) {
 			for(int y = 0; y < height; y ++) {
 				Color color = textureMap.GetPixel(x, y);
 				Tile tile = getTile(color);
@@ -28,12 +45,12 @@ public class LevelLoader : MonoBehaviour {
 					}
 				}
 			}
-		}
+		}*/
 	}
 
-	private Tile getTile(Color color) {
+	private Tile getTile(Color32 color) {
 		foreach(Tile tile in tiles) {
-			if(tile.color.Equals(color)) {
+			if(tile.color.r == color.r && tile.color.g == color.g && tile.color.b == color.b && tile.color.a == color.a) {
 				return tile;
 			}
 		}
@@ -43,7 +60,7 @@ public class LevelLoader : MonoBehaviour {
 	[System.Serializable]
 	public class Tile {
 
-		public Color color;
+		public Color32 color;
 		public GameObject obj;
 		public bool player;
 
